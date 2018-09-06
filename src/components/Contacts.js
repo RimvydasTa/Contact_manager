48d45f6fd1843fdd1e59dd097e730e89cde2b0a1
Rimvydas <tamosiunas.rimvydas@gmail.com>
2018-09-06 21:08:50 +0300

    First commit

---
+++ src/components/Contacts.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Contact from "./Contact";
+import PropTypes from 'prop-types';
+
+class Contacts extends Component {
+
+    state = {
+            contacts: [
+                {
+                    id: 1,
+                    name: 'John Doe',
+                    email: 'jdoe@email',
+                    phone: '555-555-555'
+                },
+                {
+                    id: 2,
+                    name: 'Alan Doe',
+                    email: 'alan@email',
+                    phone: '111-555-555'
+                },
+                {
+                    id: 3,
+                    name: 'Rita Doe',
+                    email: 'rita@email',
+                    phone: '555-222-555'
+                }
+            ]
+        };
+
+
+
+    render() {
+        const { contacts } = this.state;
+        return (
+            <div>
+                {contacts.map(contact => (
+                    <Contact
+                        key = {contact.id}
+                        contact={contact}
+
+                    />
+                ))}
+            </div>
+        );
+    };
+};
+
+Contacts.propTypes = {};
+
+export default Contacts;
